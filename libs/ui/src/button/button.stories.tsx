5e6a382cfe64187833bb514699e24cb675e52539
Mohamad-Ghasemilou <mohamad.ghasemilou@gmail.com>
2023-08-23 11:42:03 +0330

    add storybook to ui lib

---
+++ libs/ui/src/button/button.stories.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import Button from "./index"
+import { Meta } from '@storybook/react'
+
+
+
+export default {
+  title: 'Button',
+  component: Button,
+  // decorators: [withTests({ results })],
+  argTypes: {
+    content: {
+      defaultValue: 'Button',
+      name: 'Content',
+      type: { name: 'string', required: true },
+    }
+  },
+} as Meta
+
+const argsWithDefault = (args : any) => {
+  return {
+    ...{
+      variant : 'contained'
+    },
+    ...args,
+  }
+}
+
+export const Basic = (args : any) => {
+  return (<Button>Basic</Button>)
+}
+
+export const Contained = (args : any) => {
+  return (<Button variant={"contained"}>Contained</Button>)
+}
+
+
+export const AllOptions = (args : any) => {
+  args = argsWithDefault(args)
+  return (<Button {...args}>{args.content}</Button>)
+}
