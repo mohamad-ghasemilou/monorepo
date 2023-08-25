e831c43e18beeb1e3841ef7b862f6c4fa5ec18ed
Mohamad-Ghasemilou <mohamad.ghasemilou@gmail.com>
2023-08-25 21:39:45 +0330

    add routes

---
+++ tools/routes.ts
@@ -0,0 +1,28 @@
+const home = '/';
+const product = 'product';
+const singleProduct = product + '/:productId';
+const cart = 'cart';
+
+const routes = {
+    home,
+    product,
+    singleProduct,
+    cart
+};
+
+export default routes;
+
+export function homeRoute(query?:string):string {
+    return routes.home + (query ? `?category=${query}` : '');
+}
+
+export function productRoute(id:number):string {
+    return routes.product + '/' + id;
+}
+
+export function cartRoute():string {
+    return routes.cart;
+}
+
+
+
