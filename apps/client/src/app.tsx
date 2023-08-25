69280caacbd3dc1f398e482f2394380a102a2ba7
Mohamad-Ghasemilou <mohamad.ghasemilou@gmail.com>
2023-08-26 01:35:31 +0330

    test ci

--- apps/client/src/app.tsx
+++ apps/client/src/app.tsx
@@ -37,7 +37,7 @@ export function App() {
           path="/"
           element={
             <div>
-              aa This is the generated root route.{' '}
+              This is the generated root route.{' '}
               <Link to="/page-2">Click here for page 2.</Link>
             </div>
           }
