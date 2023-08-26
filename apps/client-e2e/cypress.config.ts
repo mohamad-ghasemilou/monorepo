129703f1b037884ba9cf6799b1b16e256599014a
Mohamad-Ghasemilou <mohamad.ghasemilou@gmail.com>
2023-08-27 01:18:17 +0330

    try to fix

--- apps/client-e2e/cypress.config.ts
+++ apps/client-e2e/cypress.config.ts
@@ -1,10 +1,8 @@
-const path = require('path');
 import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 
 export default defineConfig({
   e2e: nxE2EPreset(__dirname, {
     bundler: 'vite',
-    specPattern: path.resolve(__dirname, 'path/to/src/**/*.cy.js'),
   }),
 });
