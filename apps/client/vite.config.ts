4cec0a47156d07261e03287b70a23d622dc350c7
Mohamad-Ghasemilou <mohamad.ghasemilou@gmail.com>
2023-08-27 00:01:18 +0330

    fix absolute import

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -31,10 +31,4 @@ export default defineConfig({
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
-
-  resolve: {
-    alias: {
-      client: "/src",
-    },
-  },
 });
--- libs/data-access/src/product/product.service.ts
+++ libs/data-access/src/product/product.service.ts
@@ -1,17 +1,5 @@
 import {ApiClient} from "../api";
-// import type {ProductId, CreateProduct} from "@monorepo/feature";
-export type ProductId = number;
-
-export interface Product {
-  id: ProductId;
-  title: string;
-  price: number;
-  image: string;
-  description: string;
-  category: string;
-}
-
-export type CreateProduct = Omit<Product, "id" | "category"> & { categoryId: number }
+import type {ProductId, CreateProduct} from "@monorepo/feature";
 
 const productsUrl = "/products";
 
