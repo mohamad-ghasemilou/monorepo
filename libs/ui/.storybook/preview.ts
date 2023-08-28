4b25874c063e519ea62ea683fa1c2e53962e28b5
Mohamad-Ghasemilou <mohamad.ghasemilou@gmail.com>
2023-08-28 19:08:53 +0330

    add css base line to storybook preview

--- libs/ui/.storybook/preview.ts
+++ libs/ui/.storybook/preview.ts
@@ -1,1 +1,15 @@
+import {CssBaseline, ThemeProvider} from '@mui/material';
+import {withThemeFromJSXProvider} from '@storybook/addon-styling';
+import {lightTheme, darkTheme} from '../src/themes';
 
+export const decorators = [
+  withThemeFromJSXProvider({
+    themes: {
+      light: lightTheme,
+      dark: darkTheme,
+    },
+    defaultTheme: 'light',
+    Provider: ThemeProvider,
+    GlobalStyles: CssBaseline,
+  })
+];
--- libs/ui/src/layout/index.tsx
+++ libs/ui/src/layout/index.tsx
@@ -1,16 +1,32 @@
 import Header from "../header";
 import {ReactElement} from "react";
+import Sidebar from "../sidebar";
 
 interface Props {
   children: ReactElement
 }
+
+const items = [
+  {
+    id: "1",
+    text: "item1"
+  },
+  {
+    id: "2",
+    text: "item2"
+  },
+  {
+    id: "3",
+    text: "item3"
+  },
+]
 
 function Layout(props:Props) {
   return (
     <>
       <Header/>
       <main>
-
+        <Sidebar items={items} onItemClick={_=>{}}/>
         {props.children}
       </main>
     </>
