6938c9fb5d76703a40832bc61df714114f7a3baf
Mohamad-Ghasemilou <mohamad.ghasemilou@gmail.com>
2023-08-31 19:12:52 +0330

    fix react query cache

--- apps/client/src/app.tsx
+++ apps/client/src/app.tsx
@@ -1,30 +1,17 @@
 import {lazy, Suspense} from 'react'
-import {useQuery} from "@tanstack/react-query";
-import {ProductsService} from "@monorepo/data-access";
+import {Route, Routes, Link} from 'react-router-dom';
 import {routes, Layout} from "@monorepo/feature"
 import styles from './app.module.scss';
-// import {HomePage} from "@monorepo/feature";
-import {Route, Routes, Link} from 'react-router-dom';
-
 
 const LazyHomePage = lazy(() => import("../../../libs/feature/src/pages/home/index"));
 
 export function App() {
-  const {isLoading, error, data} = useQuery({
-    queryKey: ['allProducts'],
-    queryFn: () => ProductsService.getAll()
-  })
-
-  if (isLoading) return 'Loading...'
-  if (error) return 'An error has occurred: '
-
   return (
     <Suspense fallback={null}>
       <Layout>
         <Routes>
           <Route path={routes.home} element={<LazyHomePage/>}/>
-          <Route
-            path="/page-2"
+          <Route path={routes.singleProduct}
             element={
               <div>
                 <Link to="/">Click here to go back to root page.</Link>
--- libs/feature/src/pages/home/index.tsx
+++ libs/feature/src/pages/home/index.tsx
@@ -2,10 +2,10 @@ import {HomePage as HomePageUI} from "@monorepo/ui";
 import {useQuery} from "@tanstack/react-query";
 import {ProductsService} from "@monorepo/data-access";
 
-function HomePage() {
+export function HomePage() {
   const {isLoading, error, data} = useQuery({
     queryKey: ['allProducts'],
-    queryFn: () => ProductsService.getAll()
+    queryFn: () => ProductsService.getAll(),
   })
 
   if (isLoading) return <h1>loading products</h1> // skeleton
